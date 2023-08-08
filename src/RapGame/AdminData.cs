81774b40849355a17daebb5f53600faa25eb1f1d
Rap Therapy <126157847+RapTherapy@users.noreply.github.com>
2023-08-08 22:27:28 +0100

    Add files via upload

---
+++ src/RapGame/AdminData.cs
@@ -0,0 +1,10 @@
+﻿namespace RapGame
+{
+    public class AdminData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string Path { get; set; }
+        public string BasePath { get; set; }
+    }
+}
