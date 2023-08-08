3a4ab4e5376563a02856c9b5f7aa73846394501f
Rap Therapy <126157847+RapTherapy@users.noreply.github.com>
2023-08-08 22:31:50 +0100

    Add files via upload

---
+++ src/RapGame/Interfaces/IStudentDataReader.cs
@@ -0,0 +1,16 @@
+﻿using RapGame.Models;
+using System;
+using System.Collections.Generic;
+
+namespace RapGame.Interfaces
+{
+    public interface IStudentDataReader
+    {
+        IEnumerable<Student> GetStudents();
+        Student CreateStudent(string login, string password);
+        bool ChangePassword(Guid id, string newPassword);
+        Student? GetStudent(Guid id);
+        void UpdateStudent(Student currentStudent);
+        void DeleteStudent(Guid id);
+    }
+}
