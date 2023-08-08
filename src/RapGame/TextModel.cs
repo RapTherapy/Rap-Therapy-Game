18e163dc2f60d6ebd3bd2069ba8ce416d7fb00a4
Rap Therapy <126157847+RapTherapy@users.noreply.github.com>
2023-08-08 22:30:19 +0100

    Add files via upload

---
+++ src/RapGame/TextModel.cs
@@ -0,0 +1,17 @@
+﻿namespace ElectronRazorPage
+{
+    public class TextModel
+    {
+        public int FrameNumber { get; set; }
+        public string Text { get; set; }
+    }
+    public class TextModelFrame25: TextModel
+    {
+        public string Header { get; set; }
+    }
+
+    public class TextModelFrame136 : TextModel
+    {
+        public string Header { get; set; }
+    }
+}
